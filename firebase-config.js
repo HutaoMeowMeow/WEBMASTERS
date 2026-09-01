780725772d8eebb660b068e875864657691aba14
Gian <hutao260132@gmail.com>
2026-09-01 10:26:26 +0800

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDDWheQnrcMqb746nO9IrwadmOIFs4FmOk",
+  authDomain: "webmasters-2ce73.firebaseapp.com",
+  projectId: "webmasters-2ce73",
+  storageBucket: "webmasters-2ce73.firebasestorage.app",
+  messagingSenderId: "200671473015",
+  appId: "1:200671473015:web:8ff9484132f27bacc34572"
+};
